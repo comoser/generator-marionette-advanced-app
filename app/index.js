d79399d830281b994e2991ff7c93704fa02a85ca
David Alecrim <da@void.pt>
2015-03-19 10:49:04 +0000

    minor fix

--- app/index.js
+++ app/index.js
@@ -37,7 +37,7 @@ module.exports = yeoman.generators.Base.extend({
 
         // Have Yeoman greet the user.
         this.log(yosay(
-            'Welcome to the priceless ' + chalk.red('Marionette Advanced App') + ' generator!'
+            'Welcome to the priceless ' + chalk.blue('Marionette Advanced App') + ' generator!'
         ));
 
         var prompts = [{
